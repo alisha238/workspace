15dcf4721244175aa056194153a8e3ed96fbf6cd
alisha238 <alishain238@gmail.com>
2023-10-20 11:23:58 +0000

    dotnet project

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Entity;
+namespace DataLayer;
+public class DataAccess
+{
+    public static List <Movie> movies = new List<Movie>(){
+        new Movie{ Id=1, Name= "Toofan", Ryear= 2021, Ratings= 3},
+        new Movie{ Id=1, Name= "Ludo", Ryear= 2021, Ratings= 3},
+        new Movie{ Id=1, Name= "Gunjan", Ryear= 2021, Ratings= 3},
+        new Movie{ Id=1, Name= "Big Bull", Ryear= 2021, Ratings= 3},
+        new Movie{ Id=1, Name= "Laxmi", Ryear= 2021, Ratings= 3}
+    };
+    public List<Movie> GetMovies(){
+        return movies;
+    }
+    
+}
